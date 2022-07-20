7fe534bc45387659e6afeda37b278b527e62f18e
hrmoyes <hrmoyes@gmail.com>
2022-07-20 09:18:00 -0500

    more heroku, ports, procfile, etc

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.get('/', (req, res) => {
 // Server Listener
 ////////////////////////////////////////////
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+app.listen(PORT || 3000, () => {
 	console.log(`app is listening on port: ${PORT}`)
 })
