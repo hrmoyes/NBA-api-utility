36cd15ec86e01bb7b1c407b4dc258d3927c03c0a
hrmoyes <hrmoyes@gmail.com>
2022-07-20 09:56:38 -0500

    server.js test change 4

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const methodOverride = require('method-override')
 const playerRoutes = require('./controller/player_routes')
 const userRoutes = require('./controller/user_routes')
 const teamRoutes = require('./controller/team_routes')
+const urlHeroku = 'mongodb+srv://hrmoyes:password54@nba-dream-team.kknbmr5.mongodb.net/?retryWrites=true&w=majority'
 ////////////////////////////////////////////
 // Create our express application object
 ////////////////////////////////////////////
@@ -33,9 +34,8 @@ app.use(
 	session({
 		// secret: process.env.SECRET,
 		store: MongoStore.create({
-			mongoUrl: process.env.MONGODB_URI,
+			mongoUrl: urlHeroku,
 			secret: 'epicSecretDrewThompson',
-			touchAfter: 24 * 60 * 60
 		}),
 		saveUninitialized: true,
 		resave: false
